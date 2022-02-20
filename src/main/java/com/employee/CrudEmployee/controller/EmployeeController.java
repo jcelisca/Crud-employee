c20e3bc112adec5ad53de2a51070c792cbd929b2
jcelisca <celiss1997@gmail.com>
2022-02-20 18:07:16 -0500

    Comentarios de la funcionalidad de los métodos del controlador

--- src/main/java/com/employee/CrudEmployee/controller/EmployeeController.java
+++ src/main/java/com/employee/CrudEmployee/controller/EmployeeController.java
@@ -23,6 +23,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/employees")
+
+/**
+ * Clase controlador desde el cual se generan todas las peticiones de la api
+ */
 public class EmployeeController {
 
     @Autowired
@@ -34,11 +38,20 @@ public class EmployeeController {
     @Autowired
     IRoleJpaRepository role;
 
+    /**
+     * Método que devuelve una lista con todos lo empleados existentes en la base de datos
+     * @return Lista de empleados con sus atributos
+     */
     @GetMapping()
     public ArrayList<Employee> gEmployees(){
         return (ArrayList<Employee>) employee.findAll();
     }
 
+    /**
+     * Métododo que agrega a la base de datos un objeto de tipo empleado
+     * @param emp Objeto de tipo empleado con sus atributos
+     * @return Lista de atributos del objeto empleado con sus valores
+     */
     @PostMapping("/newemployee")
     public Employee saveEmployee(@RequestBody Employee emp){
         Employee em = new Employee(emp.getFirstName(), emp.getLastName(), emp.getEmployeeid(), emp.getRole());
@@ -52,6 +65,12 @@ public class EmployeeController {
 
     }
 
+    /**
+     * Método que actualiza los valores de un empleado existente por medio de su ID
+     * @param id Identificador del empleado que se quiere actualizar
+     * @param emp Objeto empleado con sus nuevos valores de atributos
+     * @return Lista de atributos del objeto empleado con sus valores
+     */
     @PutMapping("/update/{id}")
     public Employee updateEmployeeById(@PathVariable("id") long id, @RequestBody Employee emp){
         Optional<Employee> employeeId = employee.findById(id);
@@ -68,16 +87,31 @@ public class EmployeeController {
         return this.employee.save(e);
     }
 
+    /**
+     * Método que devulve un objeto empleado con sus valores de atributos, por medio de su ID
+     * @param id Identificador del empleado a buscar
+     * @return Lista de atributos del objeto empleado con sus valores
+     */
     @GetMapping(path = "/{id}")
     public Optional<Employee> getEmployeeById(@PathVariable("id") Long id){
         return employee.findById(id);
     }
 
+    /**
+     * Método que devulve un objeto empleado con sus valores de atributos, por medio de su nombre
+     * @param firstName Nombre del empleado a buscar
+     * @return Lista de empleados con sus valores de atributos
+     */
     @GetMapping(path = "/employee/{firstName}")
     public ArrayList<Employee> getEmployeeByFirstName(@PathVariable("firstName") String firstName){
         return (ArrayList<Employee>)employee.findByFirstName(firstName);
     }
 
+    /**
+     * Método para eliminar un empleado por medio de su ID
+     * @param id Identificador numérico del empleado a eliminar
+     * @return Mensaje con información si se pudo eliminar el empleado o no
+     */
     @DeleteMapping("/{id}")
     public String deleteEmployeeById(@PathVariable("id") Long id){
         try{
@@ -88,6 +122,11 @@ public class EmployeeController {
         }
     }
 
+    /**
+     * Método para eliminar un empleado por medio de su nombre
+     * @param firstName Nombre del empleado a ser eliminado
+     * @return Mensaje con información si se pudo eliminar el empleado o no
+     */
     @DeleteMapping("/delete/{firstName}")
     public String deleteEmployeeById(@PathVariable("firstName") String firstName){
         try{
@@ -99,26 +138,50 @@ public class EmployeeController {
         }
     }
 
+    /**
+     * Método que devuelde la lista de objetos con la información de los proyectos existentes
+     * @return Lista de proyectos existentes en la base de datos
+     */
     @GetMapping("/projects")
     public ArrayList<Project> gProjects(){
         return (ArrayList<Project>) project.findAll();
     }
 
+    /**
+     * Método para agregar un nuevo objeto de tipo proyecto a la base de datos
+     * @param proj Lista de atributos del objeto project
+     * @return Objeto project con sus valores de atributos
+     */
     @PostMapping("/projects")
     public Project saveProject(@RequestBody Project proj){
         return this.project.save(proj);
     }
 
+    /**
+     * Método que devuelve la lista de objetos tipo Role existentes en la base de datos
+     * @return Objetos Role con valores de atributos
+     */
     @GetMapping("/roles")
     public ArrayList<Role> gRoles(){
         return (ArrayList<Role>) role.findAll();
     }
 
+    /**
+     * Método que permite agregar un nuevo objeto rol a la base de datos
+     * @param rol Objeto Role con valores de atributos
+     * @return Objeto guardado con sus valores de atributos
+     */
     @PostMapping("/roles")
     public Role saveRole(@RequestBody Role rol){
         return this.role.save(rol);
     }
 
+    /**
+     * Método que permite actualizar los valores de un objeto rol, por medio de su ID
+     * @param id Identificador numérico del objeto Role a modificar
+     * @param rol Nuevos valores de atributos para el objeto Role
+     * @return Valores de atributos del objeto actualizado
+     */
     @PutMapping("/roles/update/{id}")
     public Role updateRoleById(@PathVariable("id") long id, @RequestBody Role rol){
         Optional<Role> roleId = role.findById(id);
@@ -127,6 +190,12 @@ public class EmployeeController {
         return this.role.save(r);
     }
 
+    /**
+     * Método para actualizar un objeto project por medio de su ID
+     * @param id Identificador numérico del proyecto a modificar
+     * @param proj Valores de atributos para el objeto
+     * @return Valores de atributos del objeto actualizado
+     */
     @PutMapping("/projects/update/{id}")
     public Project updateProjectById(@PathVariable("id") long id, @RequestBody Project proj){
         Optional<Project> projId = project.findById(id);
